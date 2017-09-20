0da94e56826adf696b1097c36e50a6f656e5b182
drkrieger <eplyushko@gmail.com>
2017-09-19 18:08:13 -0700

    code code chal done

---
+++ Linked_lists_find/Linked_lists_find/LinkedList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_lists_find
+{
+    class LinkedList
+    {
+        public Node head;
+        public Node curr;
+        public int count = 0;
+        public int midCount;
+        
+        public LinkedList(int data)
+        {
+            head = new Node(data);
+            curr = head;
+        }
+
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+            newNode.Data = data;
+            curr.Next = newNode;
+            curr = newNode;
+            count++;
+            if (count % 2 == 1)
+            {
+                midCount++;
+            }
+
+        }
+        public void PrintAllNodes()
+        {
+            Console.Write("Head ->");
+            Node curr = head;
+
+            while (curr.Next != null)
+            {
+                curr = curr.Next;
+                Console.Write(curr.Data);
+                Console.Write("->");
+            }
+            Console.Write("This is the end");
+        }
+        public void Printmid()
+        {
+            Console.Write("Head ->");
+            Node curr = head;
+            int start = 0;
+
+            while (start < midCount)
+            {
+                curr = curr.Next;
+                Console.Write(curr.Data);
+                Console.Write("->");
+                start++;
+            }
+            Console.Write("This is the end");
+
+        }
+    }
+}
---
+++ Linked_lists_find/Linked_lists_find/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_lists_find
+{
+    class Node
+    {
+        public Node Next { get; set; }
+        public Object Data { get; set; }
+
+        public Node(Object data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
