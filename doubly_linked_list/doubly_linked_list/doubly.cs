e3f24fba3ff47714e5f5a833e4cf5cd7fb2b30e2
drkrieger <eplyushko@gmail.com>
2017-09-22 18:04:16 -0700

    doubly

---
+++ doubly_linked_list/doubly_linked_list/doubly.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace doubly_linked_list
+{
+    class doubly
+    {
+        public Node head;
+        public Node curr;
+
+        public doubly(int data)
+        {
+            head = new Node(data);                      
+            curr = head;
+        }
+
+        public void Add(object data)
+        {
+            Node newnode = new Node(data);
+            curr.Next = newnode;
+            newnode.Prev = curr;
+            //curr = newnode;
+        }
+        public void Print()
+        {
+            curr = head;
+            while(curr.Next != null)
+            {   
+                Console.Write("->");
+                Console.Write(curr.Data);
+                curr = curr.Next;
+
+            }
+            Console.Write("->");
+            Console.Write("null");
+        }
+        public void Remove(object data)
+        {
+            //curr = head;
+            //while (curr.Next != null)
+            //{
+                if (curr.Next == data)
+                {
+                    curr.Next.Prev = null;
+                    curr.Next.Next.Prev = curr;
+                    curr.Next = curr.Next.Next;
+                }
+            curr = curr.Next; 
+            //}
+            //Console.WriteLine(curr.Next.Data);
+        }
+
+    }
+}
