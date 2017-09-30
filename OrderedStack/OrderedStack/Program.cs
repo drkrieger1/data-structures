b570e9e3536b6628b177f91f16828c9f395b11b5
drkrieger <eplyushko@gmail.com>
2017-09-29 20:30:18 -0700

    its broke

---
+++ OrderedStack/OrderedStack/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OrderedStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ordered stack \n");
+            Stack myStack = new Stack(0);
+
+            myStack.Push(3);
+            myStack.Push(5);
+            myStack.Push(1);
+            myStack.Push(4);
+            myStack.Push(2);
+
+            myStack.Print();
+            Console.Read();
+        }
+    }
+}
---
+++ OrderedStack/OrderedStack/Stack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderedStack
+{
+    class Stack
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+
+        public Stack(int data)
+        {
+            Head = new Node(data);
+            Tail = Head;
+        }
+
+        public Node Sort(int data)
+        {
+            Node curr = Head;
+
+            while(data < curr.Data)
+            {
+                if(curr.Next == null)
+                {
+                    return null;
+                }
+                curr = curr.Next;
+            }
+            return curr.Next;
+        }
+        public void Push(int data)
+        {
+            Node curr = Head;
+            Node newNode = Sort(data);
+
+            newNode.Prev = curr.Prev;
+            newNode.Next = curr;
+            curr.Prev.Next = newNode;
+            curr.Prev = newNode;
+        }
+        public void Print()
+        {
+            Node curr = Head;
+            while(curr.Next != null)
+            {
+                Console.Write("->");
+                Console.Write(curr.Data);
+                curr = curr.Next;
+            }
+            Console.Write(curr.Data);
+            Console.Write("Tail");
+        }
+        
+    }
+}
