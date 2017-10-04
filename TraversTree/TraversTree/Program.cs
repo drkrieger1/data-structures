fb6d8f18e395b81c5d8b51b7efa3fc403f8c3b5a
drkrieger <eplyushko@gmail.com>
2017-10-03 17:31:19 -0700

    challenge done

---
+++ TraversTree/TraversTree/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TraversTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data tree traversal");
+
+            Tree mytree = new Tree(1);
+
+            mytree.Left = new Tree(2);
+            mytree.Left.Left = new Tree(3);
+            mytree.Left.Right = new Tree(4);
+            mytree.Right = new Tree(5);
+            mytree.Right.Left = new Tree(6);
+            mytree.Right.Right = new Tree(7);
+
+            mytree.Travers();
+
+            Console.Read();
+        }
+    }
+}
---
+++ TraversTree/TraversTree/Tree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TraversTree
+{
+    class Tree
+    {
+        public int Data { get; set; }
+        public Tree Left { get; set; }
+        public Tree Right { get; set; }
+
+        public Tree(int data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+
+        public void Travers()
+        {
+            if(Right != null)
+            {
+                Right.Travers();
+            }
+            if(Left != null)
+            {
+                Left.Travers();
+            }
+            Console.Write(Data);
+        }
+    }
+}
