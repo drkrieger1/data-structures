658aaee71326b24ff4075de5612c5c74fde0a361
drkrieger <eplyushko@gmail.com>
2017-11-10 13:43:30 -0800

    done

--- fib/fib/Program.cs
+++ fib/fib/Program.cs
@@ -6,7 +6,40 @@ namespace fib
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Fib find Nth");
+            Fib myFib = new Fib();
+            Console.WriteLine("Enter Nth number to find");
+            myFib.FindNth(Int32.Parse(Console.ReadLine()));
+            Console.Read();
+
+        }
+    }
+    public class Fib
+    {
+        int count;
+        int f;
+
+        public void FindNth(int n)
+        {
+            f = n;
+            Console.WriteLine(Find(0, 1));
+        }
+
+        public int Find(int a, int b)
+        {
+            if (count == f)
+            {
+                return b;
+            }
+            b = a + b;
+            count++;
+            if (count == f)
+            {
+                return b;
+            }
+            a = b - a;
+            count++;
+            return Find(a, b);
         }
     }
 }
